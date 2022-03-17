adba967029705bc57ce60dc74621e7fb2c559a3b
Suraj Sankarsingh <sankarsinghsuraj@gmail.com>
2022-03-17 00:52:18 -0400

    wip - implement route protection and error handling for admin and user routes

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,12 +1,36 @@
-import { Outlet } from 'remix';
+import { Outlet, useCatch } from 'remix';
+import { requireAdmin } from '~/utils/session.server';
+import { LoaderFunction } from 'remix';
 
-type Props = {};
+type LoaderData = {
+  userId: Awaited<ReturnType<typeof requireAdmin>>;
+};
 
-export default function Admin({}: Props) {
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireAdmin(request);
+
+  const data: LoaderData = { userId };
+
+  return data;
+};
+
+export default function AdminRoute() {
   return (
     <>
       <h1>Admin</h1>
       <Outlet />
     </>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 401) {
+    return (
+      <>
+        <h1>Oh No!! It looks like you need to level up to view this page!!</h1>
+      </>
+    );
+  }
 }
--- app/routes/me.tsx
+++ app/routes/me.tsx
@@ -1,12 +1,35 @@
-import { Outlet } from 'remix';
+import { LoaderFunction, Outlet } from 'remix';
+import { useCatch } from 'remix';
+import { requireUserId } from '~/utils/session.server';
 
-type Props = {};
+type LoaderData = {
+  userId: Awaited<ReturnType<typeof requireUserId>>;
+};
 
-export default function Me({}: Props) {
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  const data: LoaderData = { userId };
+  return data;
+};
+
+export default function Me() {
   return (
     <>
       <h1>User Pages</h1>
       <Outlet />
     </>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 401) {
+    return (
+      <>
+        <h1>Oh No!! It looks like you need to login to view this page!!</h1>
+      </>
+    );
+  }
 }
