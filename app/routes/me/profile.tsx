41eda41b54cadf852e7851e1568c692261eafae4
Suraj Sankarsingh <sankarsinghsuraj@gmail.com>
2022-03-24 11:31:25 -0400

    wip - style user profile page

--- app/routes/me/profile.tsx
+++ app/routes/me/profile.tsx
@@ -17,11 +17,16 @@ export default function ProfileRoute() {
   const data = useLoaderData<LoaderData>();
 
   return (
-    <div className='flex flex-col justify-center h-screen lg:flex-row lg:justify-evenly'>
-      <div className='flex flex-col'>
-        <div>Current User Details:</div>
-        <h2>Username: {data.user?.username}</h2>
-        <h2>Email: {data.user?.email}</h2>
+    <div className='flex flex-col justify-center h-screen lg:flex-row lg:justify-evenly lg:mt-16'>
+      <div className='flex flex-col text-center mb-6'>
+        <div className='text-xl font-semibold mb-8'>Current User Details:</div>
+        <h2 className='my-4 text-lg italic'>
+          Username:
+          <span className='ml-4 text-cyan-600'>{data.user?.username}</span>
+        </h2>
+        <h2 className='my-4 text-lg italic'>
+          Email: <span className='ml-4 text-cyan-600'>{data.user?.email}</span>
+        </h2>
       </div>
       <Outlet />
     </div>
