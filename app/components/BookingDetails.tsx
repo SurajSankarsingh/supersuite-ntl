a86d23ac5b917e070c07c03e921e71d855aee4fa
Suraj Sankarsingh <sankarsinghsuraj@gmail.com>
2022-03-28 12:00:09 -0400

    wip - adjust styling on booking details page

--- app/components/BookingDetails.tsx
+++ app/components/BookingDetails.tsx
@@ -7,43 +7,64 @@ export default function BookingDetails({ booking }: BookingDetailsProps) {
   const checkOutDate = formatDate(booking!.checkOutDate);
 
   return (
-    <section className='container p-8'>
-      <div className=''>
-        <div className=''>
-          <h4 className='mb-4'>User Info</h4>
-          <p>
-            <b>Name:</b> {booking?.user?.username}
-          </p>
-          <p>
-            <b>Email:</b> {booking?.user?.email}
-          </p>
-          <p>
-            <b>Amount:</b> ${booking?.amountPaid}
-          </p>
+    <section className='p-8'>
+      <div className='container px-5 py-24 mx-auto'>
+        <hr />
+        <h2 className='sm:text-3xl text-2xl font-medium title-font mb-8 mt-2'>
+          User Info:
+        </h2>
+        <p className='text-base leading-relaxed mt-2 justify-between'>
+          <b>Name:</b>{' '}
+          <span className='ml-2 text-cyan-500 text-lg'>
+            {booking?.user?.username}
+          </span>
+        </p>
+        <p className='text-base leading-relaxed mt-2 mb-4'>
+          <b>Email:</b>
+          <span className='ml-2 text-cyan-500 text-lg'>
+            {booking?.user?.email}
+          </span>
+        </p>
 
-          <hr />
+        <hr />
 
-          <h4 className='mb-4'>Booking Info</h4>
-          <p>
-            <b>Check In:</b> {checkInDate}
-          </p>
+        <h4 className='sm:text-3xl text-2xl font-medium title-font mb-8 mt-2'>
+          Booking Info:
+        </h4>
+        <p className='text-base leading-relaxed mt-2'>
+          <b>Check In:</b>
+          <span className='ml-2 text-cyan-500 text-lg'>{checkInDate}</span>
+        </p>
 
-          <p>
-            <b>Check Out:</b> {checkOutDate}
-          </p>
+        <p className='text-base leading-relaxed mt-2'>
+          <b>Check Out:</b>
+          <span className='ml-2 text-cyan-500 text-lg'>{checkOutDate}</span>
+        </p>
 
-          <p>
-            <b>Days of Stay:</b> {booking?.daysOfStay}
-          </p>
+        <p className='text-base leading-relaxed mt-2 mb-4'>
+          <b>Days of Stay:</b>
+          <span className='ml-2 text-cyan-500 text-lg'>
+            {booking?.daysOfStay}
+          </span>
+        </p>
 
-          <hr />
+        <hr />
 
-          <h4 className='my-4'>Payment Status</h4>
-          {/* <p className={isPaid ? 'greenColor' : 'redColor'}>
+        <h4 className='sm:text-3xl text-2xl font-medium title-font mb-8 mt-2'>
+          Payment Status:
+        </h4>
+
+        <p className='text-base leading-relaxed mt-2 mb-4'>
+          <b>Amount:</b>
+          <span className='ml-2 text-cyan-500 text-lg'>
+            ${booking?.amountPaid}
+          </span>
+        </p>
+        {/* <p className={isPaid ? 'greenColor' : 'redColor'}>
                 <b>{isPaid ? 'Paid' : 'Not Paid'}</b>
               </p> */}
 
-          {/* {user && user.role === 'admin' && (
+        {/* {user && user.role === 'admin' && (
                 <>
                   <h4 className='my-4'>Stripe Payment ID</h4>
                   <p className='redColor'>
@@ -52,37 +73,39 @@ export default function BookingDetails({ booking }: BookingDetailsProps) {
                 </>
               )} */}
 
-          <h4 className='mt-5 mb-4'>Booked Room:</h4>
+        <hr />
+        <h4 className='sm:text-3xl text-2xl font-medium title-font mb-8 mt-2'>
+          Booked Room:
+        </h4>
 
-          <hr />
-          <div className='cart-item my-1'>
-            <div className='row my-5'>
-              <div className='col-4 col-lg-2'>
-                <img
-                  src={booking?.room.images[0]}
-                  alt={booking?.room.name}
-                  height={500}
-                  width={500}
-                />
-              </div>
-
-              <div className='col-5 col-lg-5'>
-                <Link to={`/rooms/${booking?.room.id}`}>
-                  {booking?.room.name}
-                </Link>
-              </div>
-
-              <div className='col-4 col-lg-2 mt-4 mt-lg-0'>
-                <p>${booking?.room.price_per_night}</p>
-              </div>
-
-              <div className='col-4 col-lg-3 mt-4 mt-lg-0'>
-                <p>{booking?.daysOfStay} Day(s)</p>
-              </div>
-            </div>
+        <div className='p-4 md:w-1/3 sm:mb-0 mb-6'>
+          <div className='rounded-lg h-64 overflow-hidden'>
+            <img
+              src={booking?.room.images[0]}
+              alt={booking?.room.name}
+              height={500}
+              width={500}
+              className='object-cover object-center h-full w-full'
+            />
           </div>
-          <hr />
+
+          <div className='text-3xl title-font mt-5 ml-2 text-cyan-500 font-semibold italic font-serif'>
+            <Link to={`/rooms/${booking?.room.id}`}>{booking?.room.name}</Link>
+          </div>
+
+          <div className='text-base leading-relaxed mt-2'>
+            <p className='ml-2 text-cyan-500 text-lg'>
+              ${booking?.room.price_per_night}
+            </p>
+          </div>
+
+          <div className='text-base leading-relaxed mt-2'>
+            <p className='ml-2 text-cyan-500 text-lg'>
+              {booking?.daysOfStay} Day(s)
+            </p>
+          </div>
         </div>
+        <hr />
       </div>
     </section>
   );
